89a524d32b7223ec20270ca3a4092bacebb7049e
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2025-09-13 21:46:05 -0500

    chore: finished exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = (string, repeat) => {
 
+    let result = '';
+
+    for (let i = 0; i < repeat; i++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
