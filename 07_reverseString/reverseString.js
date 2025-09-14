74146beb3fe6b480cb1b12590136cad020f92662
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2025-09-14 09:23:07 -0500

    chore: finish exercise 7 & passes all tests

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+// const reverseString = (reverse) => {
+//     let result = '';
+//     for (let i = reverse.length - 1; i >= 0; i--) {
+//         result += reverse.charAt(i);
+//     }
+//     return result;
+// }
 
-};
+const reverseString = (reverse) => {return reverse.split("").reverse().join("")}
 
 // Do not edit below this line
 module.exports = reverseString;
