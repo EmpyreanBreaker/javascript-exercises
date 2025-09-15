273d4e1be5a871005c9df11acd057ee4cc4ee245
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2025-09-15 18:09:39 -0500

    chore: finished exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = (a, b) => {
 
-};
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
 
+
+    let sum = 0;
+    let temp = 0;
+    if (a > b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (; a <= b; a++) {
+        sum += a;
+    };
+    return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
