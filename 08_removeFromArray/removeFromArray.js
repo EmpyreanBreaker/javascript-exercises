9494267ac4c915c035401363eb040789cac8bc2c
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2025-09-15 16:42:36 -0500

    chore: pass all tests for exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...theArgs) {
+
+    if (theArgs.length != 0) {
+        return arr.filter((value) => !theArgs.includes(value));
+    }
+    return arr;
 };
 
 // Do not edit below this line
