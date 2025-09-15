66a6c3b2c9e477e5dcae20bc674ecf9c569ee479
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2025-09-15 18:29:50 -0500

    chore: passed all test for exercise 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = (year) => {
 
+    // Every leap year is divisible by four
+    if (year % 4 === 0) {
+
+        // Leap years divisible by 100 must also be divisible by 400
+        if (year % 100 === 0 && year % 400 !== 0) {
+            return false;
+        }
+        
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line
