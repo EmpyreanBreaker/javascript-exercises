0a0a39ae14cc287854b8b483f3af9b4ee6598f87
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2025-09-28 21:48:01 -0500

    feat: add solutions for palindrom exercises

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    // First remove all punctuation and convert back to a string
+    string = string
+        .toLowerCase()
+        .split('')
+        .filter(char => /[a-z0-9]/.test(char)).join('');
+
+    let comparison = string.split('').reverse().join('');
+
+    // Return the comparison
+    return comparison === string;
 };
 
 // Do not edit below this line
