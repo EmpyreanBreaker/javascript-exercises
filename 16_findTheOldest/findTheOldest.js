55b10d1ca3fb18ae544eb1b5e388b2e5c039d186
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2025-09-28 22:17:01 -0500

    feat: add solutions for exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return person = people.sort((firstPerson, secondPerson) => {
+        // Get Ages
+        let firstPersonAge = 0;
+        let secondPersonAge = 0;
+
+        if (firstPerson.yearOfDeath == undefined) {
+            firstPersonAge = new Date().getFullYear() - firstPerson.yearOfBirth;
+        }
+        else {
+            firstPersonAge = firstPerson.yearOfDeath - firstPerson.yearOfBirth
+        }
+
+        if (secondPerson.yearOfDeath == undefined) {
+            secondPersonAge = new Date().getFullYear() - secondPerson.yearOfBirth;
+        }
+        else {
+            secondPersonAge = secondPerson.yearOfDeath - secondPerson.yearOfBirth
+        }
+
+        // Sort by oldest
+        return secondPersonAge - firstPersonAge;
+    })[0];
 
 };
 
