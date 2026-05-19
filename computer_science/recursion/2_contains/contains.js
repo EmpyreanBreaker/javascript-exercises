82c2d435d2b72528a61173f82569e4535a981eab
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2026-05-19 15:52:44 -0500

    finish contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,23 @@
-const contains = function() {
-  
+const contains = function (obj, value) {
+  // Iterate through all key-value pairs in the object
+  for (const [key, val] of Object.entries(obj)) {
+    // Special handling for NaN since NaN !== NaN
+    if (Number.isNaN(value) && Number.isNaN(val)) {
+      return true;
+    }
+    // Check if the current value matches
+    if (val === value) {
+      return true;
+    }
+    // If the value is an object (and not null), search it recursively
+    if (val !== null && typeof val === "object") {
+      if (contains(val, value)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
