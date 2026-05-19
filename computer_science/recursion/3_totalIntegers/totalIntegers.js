91267b458103a0ff4da2a6bc9fef3ab919549918
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2026-05-19 17:14:18 -0500

    feat: finish totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,39 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (data) {
+  if (!Array.isArray(data) && (data === null || typeof data !== "object")) {
+    return undefined;
+  }
+
+  const countIntegers = function (value) {
+    if (Number.isInteger(value)) {
+      return 1;
+    }
+
+    if (Array.isArray(value)) {
+      let sum = 0;
+
+      for (const item of value) {
+        sum += countIntegers(item);
+      }
+
+      return sum;
+    }
+
+    if (value !== null && typeof value === "object") {
+      let sum = 0;
+
+      for (const item of Object.values(value)) {
+        sum += countIntegers(item);
+      }
+
+      return sum;
+    }
+
+    return 0;
+  };
+
+  return countIntegers(data);
 };
-  
+
+totalIntegers();
 // Do not edit below this line
 module.exports = totalIntegers;
