454d79a9bbb533cf361501f99a50b559a7023ec2
Empyrean Breaker <152136760+EmpyreanBreaker@users.noreply.github.com>
2026-05-19 17:41:00 -0500

    feat: finish permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,28 @@
-const permutations = function() {
-  
+const permutations = function (arr) {
+  if (!Array.isArray(arr)) {
+    return undefined;
+  }
+
+  if (arr.length === 0) {
+    return [[]];
+  }
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
+
+    const remaining = [...arr.slice(0, i), ...arr.slice(i + 1)];
+
+    const remainingPermutations = permutations(remaining);
+
+    for (const permutation of remainingPermutations) {
+      result.push([current, ...permutation]);
+    }
+  }
+
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
